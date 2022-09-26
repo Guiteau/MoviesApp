1d96a2520faef2660f4ea23bc7e63bd3fdbce4c8
Guiteau <diegodam999@gmail.com>
2022-09-26 18:19:23 +0100

    Feat: Session Id, user validation and redirection to movies-list and request token (saved in local storage), done.

--- src/app/services/user-authentication.service.ts
+++ src/app/services/user-authentication.service.ts
@@ -9,19 +9,29 @@ import { token } from '../models/authentication/token';
 })
 export class UserAuthenticationService {
 
+  private _isUserLoggedIn: boolean = false;
+
   apiKey: string = "";
 
   constructor(private http: HttpClient) {
     this.apiKey = '9e511612503f5cc31b92b28a22545026';
-   }
+  }
 
-  getNewToken(): Promise<token>{
+  public get isUserLoggedIn() {
+    return this._isUserLoggedIn;
+  }
+
+  public set isUserLoggedIn(isUserLoggedIn: boolean) {
+    this._isUserLoggedIn = isUserLoggedIn;
+  }
+
+  getNewToken(): Promise<token> {
     return new Promise((resolve, reject) => {
       this.http.get(environment.URLUserAuthentication + `token/new?api_key=${this.apiKey}`).subscribe(
         (token: any) => {
           resolve(token);
         },
-        (error: any) =>{
+        (error: any) => {
           console.log('Error trying to request a new token', error);
           reject(error);
         }
@@ -29,25 +39,33 @@ export class UserAuthenticationService {
     });
   }
 
-  //not finished let it there
-  createSession(strToken: string){
-    // return new Promise((resolve, reject) => {
-    //   this.http.get(environment.URLUserAuthentication + `session/new?api_key=${this.apiKey}`).subscribe
-    // })
-    this.http.post(environment.URLUserAuthentication + `session/new?api_key=${this.apiKey}`, JSON.stringify({
-      request_token: strToken
-    })).subscribe(
-      data => {
-        return data;
+  createSessionWithLogin(strToken: string, user: string, password: string): Promise<session> {
+    return new Promise((resolve, reject) =>
+      this.http.post(environment.URLUserAuthentication + `token/validate_with_login?api_key=${this.apiKey}`, {
+        username: user,
+        password: password,
+        request_token: strToken
+      }).subscribe((session: any) => {
+        resolve(session);
       },
-      error => {
-        console.log(JSON.stringify(error.json()));
-      }
-    )
+        (error: any) => {
+          console.log('Error trying to create a new session with login', error);
+          reject(error);
+        }
+      ));
   }
 
-
-
+  createSessionId(strToken: string): Promise<session>{
+    return new Promise((resolve, reject) =>
+      this.http.post(environment.URLUserAuthentication + `session/new?api_key=${this.apiKey}`, {
+        request_token: strToken
+      }).subscribe((session: any) => {
+        resolve(session);
+      },
+        (error: any) => {
+          console.log('Error trying to request a new session id', error);
+          reject(error);
+        }
+      ));
+  }
 }
-
-
